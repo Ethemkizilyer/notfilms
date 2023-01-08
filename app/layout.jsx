de8e0d8f5ad1380b9f5bcdab5561b110d1cf7b67
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-09 02:12:21 +0300

    feat: header created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import {Inter} from "@next/font/google"
+import "@/styles/reset.css"
+import "@/styles/globals.css"
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+const interFontFamily = Inter({subsets: ["latin"]});
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='tr'>
-        <body>
-            {children}
+    <html lang="tr" className={interFontFamily.className}>
+      <body className="container">
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
         </body>
     </html>
-  )
+  );
 };
 
 export default RootLayout;
