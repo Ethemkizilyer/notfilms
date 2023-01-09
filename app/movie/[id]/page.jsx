5f6bd305538007a7740e4d77a1a42544ec038abb
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-09 04:01:24 +0300

    feat: created detail

---
+++ app/movie/[id]/page.jsx
@@ -0,0 +1,19 @@
+import { MovieContainer } from '@/containers/movie'
+import React from 'react'
+import Movies from "@/mocks/movies.json";
+import {notFound} from "next/navigation"
+
+
+const MoviePage = ({params}) => {
+    console.log(params);
+    const movieDetail= Movies.results.find(movie=>movie.id.toString()===params.id)
+
+    if(!movieDetail){
+        notFound()
+    }
+  return (
+    <MovieContainer movie={movieDetail}/>
+  )
+}
+
+export default MoviePage
